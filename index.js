6bef57a50de45d4a5310e7a95a7da681db84296f
Xi Xiao <ext-xi.xiao@elisa.fi>
2022-10-03 15:35:00 +0300

    _

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
 'use strict';
-var https = require('https');
+const https = require('https');
+const fs = require('fs');
 
-var page = 1;
-var perPage = 1;
+var fileContent = "title_en,title_zh,url,FI,rate,rateCount,clipsCount\n";
 
-var url = 'https://api.viki.io/v4/containers.json?page=' + page + '&per_page=' + perPage + '&with_paging=false&order=desc&sort=views_recent&origin_country=kr&licensed=true&app=100000a';
+var pageIndex = 1;
+var perPage = 50;
 
-https.get(url, function(res){
-    var body = '';
-    res.on('data', function(chunk){
-        body += chunk;
-    });
+function query(page, perPage){
+  var url = 'https://api.viki.io/v4/containers.json?page=' + page + '&per_page=' + perPage + '&with_paging=false&order=desc&sort=views_recent&origin_country=kr&licensed=true&app=100000a';
 
-    res.on('end', function(){
-        var more = false;
-        var result = JSON.parse(body);
-        more = result.more;
+  https.get(url, function(res){
+      var body = '';
+      res.on('data', function(chunk){
+          body += chunk;
+      });
 
-        result.response.forEach(r => {
-          console.log(r.titles.en);
-          console.log(r.clips.count);
-          console.log('1: ' + r.subtitle_completions.fi);
-          console.log('clips: ' + r.url.web);
-          console.log('rate: ' + r.review_stats.average_rating);
-          console.log('r-count: ' + r.review_stats.count);
+      res.on('end', function(){
+          var result = JSON.parse(body);
 
-        });
+          result.response.forEach(r => {
+            if(r.subtitle_completions.fi){
 
-    });
-}).on('error', function(e){
-      console.log("Got an error: ", e);
-});
+              fileContent += r.titles.en + ',';
+              fileContent += r.titles.zh + ',';
+              fileContent += r.url.web + ',';
+              fileContent += r.subtitle_completions.fi + ',';
+              fileContent += r.review_stats.average_rating + ',';
+              fileContent += r.review_stats.count + ',';
+
+              if(r.clips){
+                fileContent += r.clips.count + '\n';
+              }else{
+                fileContent += '\n';
+              }
+            }
+          });
+
+        if(result.more){
+          pageIndex++;
+          query(pageIndex, perPage);
+        }else{
+          fs.writeFile("./result.csv", fileContent, function(err) {
+              if(err) {
+                  return console.log(err);
+              }
+              console.log("The file was saved!");
+          });
+        }
+      });
+  }).on('error', function(e){
+        console.log("Got an error: ", e);
+  });
+}
+
+query(pageIndex, perPage);
